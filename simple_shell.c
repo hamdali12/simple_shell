5e4975e24f95aa4c83798531f4b829b80b4ad297
hamdali12 <134268880+hamdali12@users.noreply.github.com>
2023-11-12 22:54:31 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -88,7 +88,7 @@ void type_prompt()
 	static int first_time = 1;
 	if (first_time)
 	{
-		const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+		const char* CLEAR_SCREEN_ANSI = "\033[1;1H\033[2J";
 		write(STDERR_FILENO, CLEAR_SCREEN_ANSI, 12);
 		first_time = 0;
 	}
@@ -126,7 +126,7 @@ int main()
 		{
 			strcpy(cmd, "C:\\Windows\\System32");
 			strcpy(cmd, command);
-			execvp(cmd, parameters, envp);
+			execvp(cmd, parameters);
 		}
 		if (strcmp(command, "exit") == 0)
 		{
